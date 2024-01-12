9e44a2a7a4de737dcc5df2c3e59d6d66ad9a497a
captainsoccer <justeyallevi@gmail.com>
2024-01-12 19:10:04 +0200

    removed unused imports and added a logged allince

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -17,6 +17,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.subsystem.DriveTrain.DriveBase;
 import frc.util.humanIO.CommandPS5Controller;
 
@@ -43,6 +44,8 @@ public class RobotContainer {
     configureBindings();
     configChooser();
     configureNamedCommands();
+
+    new Trigger(DriverStation::isDSAttached).onTrue(new InstantCommand(() -> Logger.recordOutput("allince", DriverStation.getAlliance().get().toString())));
   }
 
   private void configureBindings() {
--- src/main/java/frc/robot/subsystem/DriveTrain/DriveBase.java
+++ src/main/java/frc/robot/subsystem/DriveTrain/DriveBase.java
@@ -7,7 +7,6 @@ package frc.robot.subsystem.DriveTrain;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.function.BooleanSupplier;
 
 import org.littletonrobotics.junction.AutoLog;
 import org.littletonrobotics.junction.Logger;
@@ -36,7 +35,6 @@ import edu.wpi.first.wpilibj.RobotState;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
