b07ee9c43a8540a911fe8c3eb2c7e8feb19b4ddb
jakenpun8223 <122156827+jakenpun8223@users.noreply.github.com>
2024-02-05 14:08:26 +0200

    added a command for 2nd zone
    the command uses air resistence and gravitational pull

---
+++ src/main/java/frc/robot/commands/ShooterCommands/AimShooterZone2.java
@@ -0,0 +1,47 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.ShooterCommands;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.Constants;
+import frc.robot.RobotContainer;
+import frc.robot.subsystem.Arm.Arm;
+import frc.robot.subsystem.Arm.Arm.ArmPostion;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class AimShooterZone2 extends InstantCommand {
+  private static ArmPostion target = new ArmPostion(0.4, 0.4, 0);
+  private static double StartSpeed;
+  private static double Dy;//y axis to calc the distance from speaker
+  private static double distanceToSpeaker;
+  private static boolean IsDeadZone;
+
+  public AimShooterZone2() {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(Arm.getInstance());
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    if (RobotContainer.driveBase.getPose().getTranslation().getY() < Constants.ArmConstants.SpeakerIsCenterRatioBottomLocation) {
+      IsDeadZone = true;
+          Dy = Constants.ArmConstants.SpeakerBottomLocationY
+     + Constants.ArmConstants.SpeakerIsCenterRatioReverse * Constants.ArmConstants.SpeakerIsCenterRatioBottomLocation;
+    }
+    else{
+      IsDeadZone = false;
+          Dy = Constants.ArmConstants.SpeakerBottomLocationY
+     + Constants.ArmConstants.SpeakerIsCenterRatioReverse * RobotContainer.driveBase.getPose().getTranslation().getY();
+    }    
+     distanceToSpeaker = Math.sqrt(Math.pow(RobotContainer.driveBase.getPose().getTranslation().getX() - Constants.SpeakerTranslation.getX(), 2) + Math.pow(Dy, 2));
+
+     target.rotation = Math.atan(3);//insert
+     Arm.getInstance().moveShooterToPose(target);
+     
+  }
+}
