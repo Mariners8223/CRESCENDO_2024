8408505ada7feb1a85dc0de49155c942f1c749f3
jakenpun8223 <122156827+jakenpun8223@users.noreply.github.com>
2024-02-19 20:10:56 +0200

    ani gilad 6

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -97,12 +97,13 @@ public class RobotContainer {
     // driveController.cross().onTrue(new MoveToFree());
     // driveController.square().onTrue(new MoveToHome());
     // driveController.cross().onTrue(new InstantCommand(() -> Arm.getInstance().getShooterSub().setShooterPower(0.5))).onFalse(new InstantCommand(() -> Arm.getInstance().getShooterSub().stopMotors()));
-    var QuickAim = new QuikAim();
+    // var QuickAim = new QuikAim();
+    var collect = new Collect();
 
-    driveController.cross().onTrue(QuickAim);
+    // driveController.cross().onTrue(QuickAim);
     // driveController.cross().onTrue(new InstantCommand(() -> isQuickAiming = !isQuickAiming));
     driveController.square().onTrue(new IntakeToFloor());
-    driveController.circle().onTrue(new Collect());
+    driveController.circle().onTrue(collect).onFalse(new InstantCommand(() -> collect.cancel()));
     driveController.cross().onTrue(new ShootToAmp());
     // driveController.triangle().onTrue(new Shoot()).onFalse(new InstantCommand(() -> {QuickAim.cancel(); driveBase.isControlled = false;}));
 
