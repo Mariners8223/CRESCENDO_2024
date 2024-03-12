e5cd07430d52e0270ff732f00f3d43efc9bd5303
captainsoccer <justeyallevi@gmail.com>
2024-03-12 14:08:50 +0200

    chnaged source collect duo to the location of the beambraker

--- src/main/java/frc/robot/commands/IntakeCommands/SourceCollect.java
+++ src/main/java/frc/robot/commands/IntakeCommands/SourceCollect.java
@@ -4,10 +4,8 @@
 
 package frc.robot.commands.IntakeCommands;
 
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.commands.IntakeCommands.Collect.Collect3;
 import frc.robot.commands.armCommands.MoveToSourceCollection;
 import frc.robot.subsystem.Arm.Arm;
 import frc.robot.subsystem.Arm.Intake.Intake;
@@ -21,7 +19,8 @@ public class SourceCollect extends SequentialCommandGroup {
   public SourceCollect() {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
-    addCommands(new MoveToSourceCollection(), new InnerSourceCollect1(), new InnerSourceCollect2(), new Collect3());
+    addCommands(new MoveToSourceCollection(), new InnerSourceCollect1(), new InnerSourceCollect2(), new InnerSourceCollect3(),
+     new InnerSourceCollect4());
   }
 
   public class InnerSourceCollect1 extends Command {
@@ -47,19 +46,63 @@ public class SourceCollect extends SequentialCommandGroup {
   }
   public class InnerSourceCollect2 extends Command {
     private static Intake intake;
-    private static Shooter shooter;
 
     public InnerSourceCollect2(){
       intake = Arm.getInstance().getIntakeSub();
-      shooter = Arm.getInstance().getShooterSub();
     }
 
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
-      shooter.stopMotors();
+      System.out.println("Note is in");
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+      return !intake.getLaserReading(); 
+    }
+  }
+  public class InnerSourceCollect3 extends Command {
+    private static Intake intake;
+
+    public InnerSourceCollect3(){
+      intake = Arm.getInstance().getIntakeSub();
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+      System.out.println("Note is in 2");
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+      return intake.getLaserReading(); 
+    }
+  }
+  public class InnerSourceCollect4 extends Command {
+    private static Intake intake;
+    private static Shooter shooter;
+    
+    public InnerSourceCollect4(){
+      intake = Arm.getInstance().getIntakeSub();
+      shooter = Arm.getInstance().getShooterSub();
+    }
+
+    @Override
+    public void initialize() {
+      shooter.setShooterPower(-0.5);
+      intake.setMotor(-0.6);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+      System.out.println("Note is in position");
       intake.stopMotor();
-      intake.setMotor(0.2);
+      shooter.stopMotors();
+      intake.setIsGamePieceDetected(true);
     }
 
     // Returns true when the command should end.
