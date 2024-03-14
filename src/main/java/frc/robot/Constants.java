7f3293d61a9b6b1cf32be92437e809edb9e02170
captainsoccer <justeyallevi@gmail.com>
2024-03-14 19:04:03 +0200

    ok, so here is a story, u know pid well i chage everything

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -154,25 +154,25 @@ public class Constants {
             public static final int mainMotorID = 15;
             public static final int secondaryMotorID = 16;
 
-            public static final PIDFGains mainPID = new PIDFGains(10, 0.02, 0, 0, 0.005, 0.02);
-            public static final PIDFGains secondaryPID = new PIDFGains(3.5, 0, 0, 0, 0.01, 0.002);
+            public static final PIDFGains mainPID = new PIDFGains(0.005, 0, 0, 0, 0.005, 0.04); //i was 0.2 d was 0.001
+            public static final PIDFGains secondaryPID = new PIDFGains(0.005, 0, 0, 0, 0.01, 0.002);
 
             public static final boolean mainInverted = false;
             public static final boolean secondaryInverted = false;
 
-            public static final double mainZeroOffset = 0.7092497 - 0.25;//0.4647;
-            public static final double secondaryZeroOffset = 0.344;
+            public static final double mainZeroOffset = 0.464425862;//0.4647;
+            public static final double secondaryZeroOffset = 0.598 - 0.25;
             // public static final double mainZeroOffset = 0;
             // public static final double secondaryZeroOffset = 0;
 
             public static final double[] mainSoftLimits = new double[]{0.3206787109375, -0.03125};//was 0.35, -0.04 // 0.3206787109375
             public static final double[] secondarySoftLimits = new double[]{0.5560302734375, 0.007};
 
-            public static final double[] mainMaxOutputs = new double[]{0.45, -0.25};
-            public static final double[] secondaryMaxOutputs = new double[]{0.5, -0.35};
+            public static final double[] mainMaxOutputs = new double[]{0.1, -0.05}; //0.45, -0.25
+            public static final double[] secondaryMaxOutputs = new double[]{0.1, -0.15}; //0.5, -0.35
  
             public static final double mainConversionFactor = 150;
-            public static final double secondaryConversionFactor = 121.5;
+            public static final double secondaryConversionFactor = 81; //121.5
             public static final boolean mainEncoderInverted = false;
             public static final boolean secondaryEncoderInverted = false;
 
