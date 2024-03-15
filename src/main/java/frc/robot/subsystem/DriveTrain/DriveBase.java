4bf8af8cfe57383fd88cbeb383557578caf4bc02
captainsoccer <justeyallevi@gmail.com>
2024-03-15 20:34:40 +0200

    removed unused imports and fixed the disabled exit function

--- src/main/java/frc/robot/subsystem/DriveTrain/DriveBase.java
+++ src/main/java/frc/robot/subsystem/DriveTrain/DriveBase.java
@@ -33,7 +33,6 @@ import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.RobotState;
-import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
