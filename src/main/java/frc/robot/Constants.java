718383b5ca36d983ed9d1f3616117a83eba59b93
captainsoccer <justeyallevi@gmail.com>
2024-03-16 18:04:04 +0200

    made all the note collectors adaptive to auto collect

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -343,7 +343,7 @@ public class Constants {
             public static final double pathErrorSpikeTolerance = 1; //the max postion spike before path planner replans the path
 
             public static final PIDFGains thetaPID = new PIDFGains(5, 0.0, 0.0); //the pid gains for the PID Controller of the robot angle, units are radians
-            public static final PIDFGains XYPID = new PIDFGains(5, 0.0, 1); //the pid gains for the pid controller of the robot's postion (xy)
+            public static final PIDFGains XYPID = new PIDFGains(4, 0.0, 0.9); //the pid gains for the pid controller of the robot's postion (xy)
         }
 
         public static class SwerveModule{
